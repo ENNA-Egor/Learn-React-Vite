bd873ba688d07503761af5ed01c9e54a8b868f60
ENNA-Egor <e.yudakov@gmail.com>
2023-10-12 17:09:58 +0300

    Пробую подключение модулей

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import TabS from './module/Tabset'
 import './App.css'
 
 function App() {
@@ -9,32 +8,21 @@ function App() {
 
   return (
     <>
-    <div>
-      <h1>Привет</h1>
+    <div class="tablinks">
+    <div class="divtablink">
+      <button class="home  tablink"  onclick="openPage('.home', 'TabHome')" >Приход</button>
     </div>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)} className='button'>
-          count is {count}
-        </button>
-        <button onClick={() => setCountEnd((countEnd) => countEnd - 2)} className='button'>
-          countEnd is {countEnd}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+    <div class="divtablink">
+      <button class="tablink news" onclick="openPage('.news', 'TabNews')" >Остатки</button>
+    </div>
+      <div class="divtablink">
+      <button class="tablink contact" onclick="openPage('.contact', 'TabContact')" id="defaultOpen">Выдать</button>
+    </div>
+      <div class="divtablink">
+      <button class="tablink about" onclick="openPage('.about', 'TabAbout')">Личная карточка</button>
+    </div>
+  </div>
+  {/* <TabS/> */}
     </>
   )
 }
