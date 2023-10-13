e4b98ff3057ef723079dd34355c38e54c199d38d
ENNA-Egor <e.yudakov@gmail.com>
2023-10-13 14:16:58 +0300

    Добавил два элемента на вторую вкладку

--- src/module/Leftovers.jsx
+++ src/module/Leftovers.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 function Leftovers (){
 return (
   <>
-  <h2>Hello Leftovers</h2>
-  <h3>News</h3>
-  <p>Some news this fine day!</p>
+  <h2>Остатки</h2>
+  <form name="form2" id="form2" action="">
+        <div>
+      <button type="button" class="btn btn-ost btn-success m-2">Остатки</button> 
+    </div>
+    <div>
+        <select name="subject2" id="subject2" class="btn btn-success m-2">
+          <option value="" selected="selected">Выберите наименование СИЗ</option>
+        </select>
+      </div>
+      </form>
   </> 
  )
 }
