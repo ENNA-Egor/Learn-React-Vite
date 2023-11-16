af9dadece519f0455a574c3ee96ffffd6e318926
ENNA-Egor <e.yudakov@gmail.com>
2023-11-16 15:18:21 +0300

    почистил код

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,12 @@ import './style/css/min/Index.min.css'
 import Tabset from'./module/Tabset'
 
 function App() {
-  const [count, setCount] = useState(10)
-  const [countEnd, setCountEnd] = useState(10)
-
-  // function openPage(pageName, pageName1, elmnt, color) {
-
-    // alert (pageName);
-    // alert (pageName1);
-   function openPage(event) {
-    // // Hide all elements with class="tabcontent" by default */
-     alert ('Ok');
-     alert (event.target);
-     console.log (event.target);
-     console.log (event.target.className);
-    // console.log(event.target.className);
-    if (event.target.className === 'home'){
-      console.log ('Ok home');
-    } else {
-      console.log('No');
-    }
-    // var i, tabcontent, tablinks;
-    // tabcontent = document.getElementsByClassName("tabcontent");
-    // for (i = 0; i < tabcontent.length; i++) {
-    //   tabcontent[i].style.display = "none";
-     }
-
   
-    // // // Show the specific tab content
-    // document.getElementById(pageName1).style.display = "block";
+  function openPage(pageName, pageName1, elmnt, color) {
+
+
+   }
   
-    // // // Add the specific color to the button used to open the tab content
-    // let elem = document.querySelector(pageName);
-    // let elemAll = document.querySelectorAll('.tablink');
-    // for(i=0; i< elemAll.length; i++){
-    //   elemAll[i].classList.remove('active');
-    // }
-    // elem.classList.add('active');
-  // }
-  
-  // Get the element with id="defaultOpen" and click on it
-  // document.getElementById("defaultOpen").click();
 
 
   return (
