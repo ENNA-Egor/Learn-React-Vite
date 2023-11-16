c88d641679064a2e283ea3ee4fea4db39f58612b
ENNA-Egor <e.yudakov@gmail.com>
2023-11-16 14:38:47 +0300

    16/11/2023

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,27 @@ function App() {
   const [count, setCount] = useState(10)
   const [countEnd, setCountEnd] = useState(10)
 
-  function openPage(pageName, pageName1, elmnt, color) {
+  // function openPage(pageName, pageName1, elmnt, color) {
 
-    alert (pageName);
-    alert (pageName1);
-  // function openPage(event) {
+    // alert (pageName);
+    // alert (pageName1);
+   function openPage(event) {
     // // Hide all elements with class="tabcontent" by default */
-    // alert ('Ok');
-    // alert (event);
+     alert ('Ok');
+     alert (event.target);
+     console.log (event.target);
+     console.log (event.target.className);
     // console.log(event.target.className);
-    // if (event.target.className === 'home'){
-    //   alert ('Ok');
-    // } else {
-    //   alert('No');
-    // }
+    if (event.target.className === 'home'){
+      console.log ('Ok home');
+    } else {
+      console.log('No');
+    }
     // var i, tabcontent, tablinks;
     // tabcontent = document.getElementsByClassName("tabcontent");
     // for (i = 0; i < tabcontent.length; i++) {
     //   tabcontent[i].style.display = "none";
-    // }
+     }
 
   
     // // // Show the specific tab content
@@ -40,7 +42,7 @@ function App() {
     //   elemAll[i].classList.remove('active');
     // }
     // elem.classList.add('active');
-  }
+  // }
   
   // Get the element with id="defaultOpen" and click on it
   // document.getElementById("defaultOpen").click();
@@ -51,7 +53,7 @@ function App() {
     <h1 class="text-success d-flex justify-content-center mt-3">Выдача и учёт спецодежды.</h1>
     <div class="tablinks">
     <div class="divtablink">
-      <button onClick={openPage} class="home  tablink"  onclick="openPage('.home', 'TabHome')" >Приход</button>
+      <button onClick={openPage} class="home tablink"  onclick="openPage('.home', 'TabHome')" >Приход</button>
     </div>
     <div class="divtablink">
       <button onClick={openPage} class="news tablink" onclick="openPage('.news', 'TabNews')" >Остатки</button>
