9f359da47c2f871d8aa17128b24dffc2bca9fd7d
ENNA-Egor <e.yudakov@gmail.com>
2023-11-17 10:15:46 +0300

    Замутил начало табуляции

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,29 @@ import Tabset from'./module/Tabset'
 
 function App() {
   
-  function openPage(pageName, pageName1, elmnt, color) {
+  function openPage(event) {
+    let evValue = (event.target.className).slice(0,7);
+      alert('click    '+ evValue);
 
+    //   let evValue = (event.target.className).slice(0,5);
+    // if (evValue =='tab-h') {
+    //   let dataTab = event.target.getAttribute('data-tab');
+    //   let tabH = document.getElementsByClassName('tab-h');
+    //   for (let i = 0; i < tabH.length; i++) {
+    //     tabH[i].classList.remove('active');
+    //   }
+    //   event.target.classList.add('active');
+    //   var tabBody = document.getElementsByClassName('tab-b');
+    //   for (var i = 0; i < tabBody.length; i++) {
+    //     if (dataTab == i) {
+    //       tabBody[i].classList.remove('hide');
+    //       tabBody[i].classList.add('visible');
+    //     } else {
+    //       tabBody[i].classList.remove('visible');
+    //       tabBody[i].classList.add('hide');
+    //     }
+    //   }
+    // }
 
    }
   
@@ -19,16 +40,16 @@ function App() {
     <h1 class="text-success d-flex justify-content-center mt-3">Выдача и учёт спецодежды.</h1>
     <div class="tablinks">
     <div class="divtablink">
-      <button onClick={openPage} class="home tablink"  onclick="openPage('.home', 'TabHome')" >Приход</button>
+      <button onClick={openPage} class="tablink home"  data-tab ="0">Приход</button>
     </div>
     <div class="divtablink">
-      <button onClick={openPage} class="news tablink" onclick="openPage('.news', 'TabNews')" >Остатки</button>
+      <button onClick={openPage} class="tablink news" data-tab ="1">Остатки</button>
     </div>
       <div class="divtablink">
-      <button onClick={openPage} class="contact tablink" onclick="openPage('.contact', 'TabContact')" id="defaultOpen">Выдать</button>
+      <button onClick={openPage} class="tablink contact"  data-tab ="2">Выдать</button>
     </div>
       <div class="divtablink">
-      <button onClick={openPage} class="about tablink" onclick="openPage('.about', 'TabAbout')">Личная карточка</button>
+      <button onClick={openPage} class="tablink about" data-tab ="3" >Личная карточка</button>
     </div>
   </div>
   <Tabset/>
