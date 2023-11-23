24567d24d649f6a94b7b0cadb88d54745a37d964
ENNA-Egor <e.yudakov@gmail.com>
2023-11-23 16:14:52 +0300

    Добавил селект в карточку

--- src/module/Personcard.jsx
+++ src/module/Personcard.jsx
@@ -5,6 +5,8 @@ return (
   <>
   <div id="TabAbout" className="tabcontent hide">
   <h2>Личная карточка</h2>
+  <select name="firstname" id=""></select>
+  <select name="lastname" id=""></select>
   </div>
   </>
  )
