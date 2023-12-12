3940214fe68cbaf3c682ad9b1df5528f39ab5d87
ENNA-Egor <e.yudakov@gmail.com>
2023-12-12 12:56:40 +0300

    Промежуток

--- src/module/Сoming.jsx
+++ src/module/Сoming.jsx
@@ -46,32 +46,39 @@ const subjectObject = {
 
 class Coming extends Component {
   
-  constructor (props){
-    super(props);
-    this.state ={
-      subject: '',
-      topic: '',
-      chapter: '',
+    constructor(props) {
+      super(props);
+      this.state = {
+        subjects: Object.keys(subjectObject),
+        topics: [],
+        chapters: []
+      };
+    }
+  
+  
+    handleSubjectChange = (event) => {
+      const selectedSubject = event.target.value;
+      const selectedTopic = Object.keys(subjectObject[selectedSubject])[0]; // Default to the first topic
+      const selectedChapter = subjectObject[selectedSubject][selectedTopic][0]; // Default to the first chapter
+      this.setState({
+        topics: Object.keys(subjectObject[selectedSubject]),
+        chapters: subjectObject[selectedSubject][selectedTopic],
+        selectedSubject,
+        selectedTopic,
+        selectedChapter
+      });
     };
-    this.handleChange = this.handleChange.bind(this);
-  }
   
-  
-  handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
-  
+    handleTopicChange = (event) => {
+      const selectedTopic = event.target.value;
+      const selectedChapter = subjectObject[this.state.selectedSubject][selectedTopic][0]; // Default to the first chapter
+      this.setState({
+        chapters: subjectObject[this.state.selectedSubject][selectedTopic],
+        selectedTopic,
+        selectedChapter
+      });
+    };
   render(){
-    
-    const arrSubject = Object.keys(subjectObject)
-    const arrSubSubject = Object.keys(subjectObject[this.state.subject])
-    const arrSubSubSubject = Object.keys(subjectObject['Летний костюм']['44-46'][0])
-    // console.log(arrSubSubject);
-    console.log(arrSubSubSubject);
-    
-
-
-
 return (
   <div id="TabHome" className="tabcontent  visible">
     <h2>Приход</h2>
@@ -80,28 +87,18 @@ return (
           <input type="date" className="btn btn-success m-2"/>
         </div>
         <legend className="text-primary fs-5" > Наименование: 
-        <select value={this.state.subject} onChange={this.handleChange} name="subject" id="subject" className="btn btn-success m-2">
-          {/* <option value="" disabled selected></option> */}
-       { arrSubject.map((el)=> 
-      <option key={el} value={el}> {el} </option>
-      ) }
+        <select name="subject" id="subject" className="btn btn-success m-2" value={this.state.selectedSubject} onChange={this.handleSubjectChange}>
+          {this.state.subjects.map(subject => <option key={subject} value={subject}>{subject}</option>)}
         </select>
         <legend className="text-primary fs-5"> Размер:
-         <select value={this.state.topic} onChange={this.handleChange} name="topic" id="topic" className="btn btn-success m-2" >
-         <option value="" ></option> 
-         { arrSubSubject.map((el)=> 
-      <option key={el} value={el}> {el} </option>
-      ) }
-    
+        <select name="topic" id="topic" className="btn btn-success m-2" value={this.state.selectedTopic} onChange={this.handleTopicChange}>
+          {this.state.topics.map(topic => <option key={topic} value={topic}>{topic}</option>)}
         </select>
       </legend>
         <legend className="text-primary fs-5"> Рост:
-          <select name="chapter" id="chapter" className="btn btn-success m-2">
-          <option value="" ></option>
-          {/* { arrSubSubSubject.map((el)=> 
-      <option key={el} value={el}> {el} </option>
-      ) } */}
-          </select>
+        <select name="chapter" id="chapter" className="btn btn-success m-2" value={this.state.selectedChapter} onChange={this.handleChapterChange}>
+          {this.state.chapters.map(chapter => <option key={chapter} value={chapter}>{chapter}</option>)}
+        </select>
         </legend>
         </legend>
         <legend className="text-primary fs-5">Количество
