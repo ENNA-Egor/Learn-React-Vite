f63c0f3a2cab0cf28de349c285e96c237dc864f5
ENNA-Egor <e.yudakov@gmail.com>
2023-12-18 17:53:46 +0300

    Добавил кнопик в остатки

--- src/module/Leftovers.jsx
+++ src/module/Leftovers.jsx
@@ -15,7 +15,7 @@ class Leftovers extends Component {
     this.setState({
       selectedSubject,
     });
-    alert(selectedSubject);
+    // alert(selectedSubject);
   };
   
   
@@ -26,13 +26,16 @@ class Leftovers extends Component {
     <h2>Остатки</h2>
       <form name="form2" id="form2" action="">
         <div>
-          <div>
-              <button type="button" className="btn btn-ost btn-success m-2">Остатки</button>
-          </div>
           <span className="text-primary fs-5">Наименование: </span>
             <select name="subject2" id="subject2" className="btn btn-success m-2" value={this.state.selectedSubject} onChange={this.handleSubjectChange}>
               {this.state.subjects2.map(subject => <option key={subject} value={subject}>{subject}</option>)}
             </select>
+          <div>
+              <button type="button" className="btn btn-ost btn-success m-2">Сформировать</button>
+          </div>
+          <div>
+              <button type="button" className="btn btn-ost btn-success m-2">Выгрузить в Эксель</button>
+          </div>
         </div>
       </form>
   </div>
